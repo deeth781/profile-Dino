f6b3cae834bc6588b2946c88833407979e699761
tài code <109515588+deeth781@users.noreply.github.com>
2022-11-07 18:52:29 +0700

    Update H4X.py

--- H4X.py
+++ H4X.py
@@ -78,7 +78,7 @@ def menu():
 def main():
     menu()
     while(True):
-        cnc = input('''Input :''')
+        cnc = Lệnh By Tài('''Lệnh By Tài :''')
         if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
             ()
         elif cnc == "layer4" or cnc == "LAYER4" or cnc == "L4" or cnc == "l4":
